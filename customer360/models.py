36be52fb2a1788f6becd2bd956c400350965856a
Nekina <joshuaacusta@gmail.com>
2025-07-15 07:56:04 -0400

    Added social media as new interaction channel

--- customer360/models.py
+++ customer360/models.py
@@ -21,6 +21,7 @@ class Interaction(models.Model):
         ('sms', 'SMS'),
         ('email', 'Email'),
         ('letter', 'Letter'),
+        ('social_media', 'Social Media'),
     ]
     DIRECTION_CHOICES = [ # Max char length = 10
         ('inbound', 'Inbound'),
